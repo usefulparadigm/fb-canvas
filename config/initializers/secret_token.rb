ac318ea588aa4f4b21b478072cef1a77f15593ba
Sukjoon Kim <sjoonk@gmail.com>
2011-11-24 14:52:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbCanvas::Application.config.secret_token = '583eff78bc37f0a91f4515d267c2529f2ab0c65e5f38cdad658a3c97aa744e38e33125d60c6636fa73993f0a0e2ef8ca47df60bd4dbfb73b246155b15a97a393'
