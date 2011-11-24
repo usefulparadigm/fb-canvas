93cd97dc16a03f63b5d496c8b874ac72d3b91177
Sukjoon Kim <sjoonk@gmail.com>
2011-11-24 15:31:47 +0900

    add welcome index

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.0.10'
 
 gem 'sqlite3'
 
+gem 'koala'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
---
+++ config/initializers/app_config.rb
@@ -0,0 +1,8 @@
+AppConfig = {}
+
+
+# Facebook
+
+FB_APP_ID = "225226567548351"
+FB_APP_SECRET = "4ef635664c2a0e2aa783fefea47af91a"
+FB_APP_NAMESPACE = "fb-canvas"
